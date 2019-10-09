8d1dbfb980213f07fe606ffd5b66c55e3f955d48
Steven Murray <steven.murray@curtin.edu.au>
2019-10-09 00:45:28 -0400

    Add Data to __init__

--- pydftools/__init__.py
+++ pydftools/__init__.py
@@ -6,7 +6,7 @@ __author__ = """Steven Murray"""
 __email__ = "steven.murray@curtin.edu.au"
 __version__ = "0.1.0"
 
-from .dffit import DFFit
+from .dffit import DFFit, Data
 from . import model
 from .mockdata import mockdata
 from . import plotting
